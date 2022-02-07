f4ffc1382186318f8ab096184e516a81dd685c25
Gukss <dongkuk97@naver.com>
2022-02-07 19:46:23 +0900

    if 중복 제거 / for문 안에서는 continue로 제거한다.

--- baekjoon/1316/1316.py
+++ baekjoon/1316/1316.py
@@ -7,14 +7,13 @@ def check(string):
   # 문자열 길이만큼 반복
   for i in range(len(string) - 1):
     text = string[i]
-    # 다음 문자가 현재 문자랑 다르면 0으로 바꾸고
-    if (text != string[i+1]):
-      if(alph[ord(text) - 97] == 1):
-        alph[ord(text) - 97] = 0
-        if(alph[ord(string[i+1]) - 97] == 0):
-          return
-      elif(alph[ord(text) - 97] == 0):
-        return
+    if (text == string[i+1]):
+      continue
+    if(alph[ord(text) - 97] == 0):
+      return
+    alph[ord(text) - 97] = 0
+    if(alph[ord(string[i+1]) - 97] == 0):
+      return
   count += 1
   
 for i in range(num):
