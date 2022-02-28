fd76f9da842fb1a9fe5a3fb91aa9bd9c857062ee
Gukss <dongkuk97@naver.com>
2022-02-28 10:18:36 +0900

    완료

---
+++ baekjoon/15651/15651.py
@@ -0,0 +1,14 @@
+import sys
+n, m = map(int, sys.stdin.readline().rstrip().split())
+numbers = []
+
+def dfs():
+  if len(numbers) == m:
+    print(' '.join(map(str, numbers)))
+    return
+  for i in range(1, n+1):
+    numbers.append(i)
+    dfs()
+    numbers.pop()
+
+dfs()
